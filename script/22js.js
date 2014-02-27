7bdc0df7d7954e4a63793f94673892a7cbe8c968
sixfngers <dmc@davidcaneso.com>
2014-02-26 18:11:44 -0800

    ad

--- script/22js.js
+++ script/22js.js
@@ -36,10 +36,10 @@ function setupNav()
 {
 
 	// $('#nav ul li #about-link').on('click', function(){
-	$("a[href='#about']").click(function() {
-		e.preventDefault();
+	$("a[href='#about']").click(function(e) {
 		$('html,body').animate({scrollTop: $(this).offset().top}, 500);
 		showSiteSection($("#about-section"), $("#about-section-wrapper"));
+		e.preventDefault();
     });
 
     
