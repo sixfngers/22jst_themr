5e72eba63ad6b15f3b0f602a77333d3e7b7e5184
sixfngers <dmc@davidcaneso.com>
2014-04-22 16:16:17 -0700

    added support for photosets
    added support for full width photosets

--- script/22js.js
+++ script/22js.js
@@ -15,6 +15,75 @@ log = ( window.location.href.indexOf('22new.tumblr') > -1 || window.location.hre
 
 log('debug dev events');
 
+var photosetLayouts = [];
+
+function adjustIFrames() {   
+  var iFrames = $('.html_photoset iframe');
+
+  function loopIFrames() {
+    // Iterate through all iframes in the page.
+    for (var i = 0, j = iFrames.length; i < j; i++) {
+      var iFrame = $(iFrames[i]);
+      adjustIFrame(iFrame);
+    }
+  }
+
+  // Check if browser is Safari or Opera.
+  if ($.browser.safari || $.browser.opera) {
+    // Start timer when loaded.
+    $('.html_photoset iframe').load(function() {
+      setTimeout(loopIFrames, 0);
+    });
+
+    // Safari and Opera need something to force a load.
+    for (var i = 0, j = iFrames.length; i < j; i++) {
+      var iFrame = $(iFrames[i]);
+      var iSource = iFrame.attr('src');
+      iFrame.attr('src', '');
+      iFrame.attr('src', iSource);
+    }
+  } else {
+    // For other good browsers.
+    $('.html_photoset iframe').load(function() {
+      adjustIFrame(this);
+    });
+  }
+}
+
+function adjustIFrame(iframe) {
+  var layout = $.parseJSON($(iframe).closest('.photoset-wrapper').attr('rel'));
+  var gutter = 0;
+  var marginHeight = 10;
+  
+  $(iframe).contents().find('.photoset_row').width("100%");
+  var width = $(iframe).contents().find('.photoset_row').width();
+  var newiFrameHeight = 0;
+  for (i = 0; i < layout.length; i++) {
+    var $img = $(iframe).contents().find('.photoset_row').eq(i).children('a').children('img');
+    var numPics = layout[i];
+    var newWidth = Math.floor((width - ((numPics - 1) * gutter)) / numPics);
+    var newHeight = $img.closest('.photoset_row').height() * (newWidth / $img.width());
+    newiFrameHeight += newHeight;
+    
+    // adjust so the margin between the photos is taken into account
+    // only relevent on the photos that are not the top rows
+    if(i > 0)
+      newiFrameHeight += marginHeight;
+    
+    $img.closest('.photoset_row').height(newHeight+'px');
+    $img.width(newWidth+'px');
+  }
+  $(iframe).height(newiFrameHeight);
+
+  // Replace with high res if available and needed
+  var $images = $(iframe).contents().find('.photoset_row').children('a').children('img');
+  for (j = 0; j < $images.length; j++) {
+    var $curImage = $($images[j]);
+    var newSrc = $curImage.parent('a').attr('href');
+    if (newSrc != undefined && newSrc != '') $curImage.attr('src', newSrc);
+  }
+}
+
 
 var detectViewPort = function()
 {
@@ -30,6 +99,14 @@ $(window).resize(function () {
 $(document).ready(function(){
 	detectViewPort();
 	setupNav();
+	
+	$(".html_photoset").each(function(i) {
+	    var photoset_id = $(this).attr('id').substr(9);
+	    
+	    $(this).width("100%").children('iframe').width("100%");
+	  });
+
+	  adjustIFrames();
 });
 
 function setupNav()
